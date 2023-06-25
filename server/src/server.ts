d7e70dd7735ed50adb99259fabe295c08de42a40
yokomotod <yokomotod@gmail.com>
2023-06-25 21:33:10 +0900

    rename to bqls

--- server/src/server.ts
+++ server/src/server.ts
@@ -100,7 +100,7 @@ connection.onDidChangeConfiguration(change => {
 		documentSettings.clear();
 	} else {
 		globalSettings = <ExampleSettings>(
-			(change.settings.languageServerExample || defaultSettings)
+			(change.settings.bqls || defaultSettings)
 		);
 	}
 
@@ -116,7 +116,7 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
 	if (!result) {
 		result = connection.workspace.getConfiguration({
 			scopeUri: resource,
-			section: 'languageServerExample'
+			section: 'bqls'
 		});
 		documentSettings.set(resource, result);
 	}
