3f523ba6d4b78032aa9bdae4c0f27da4708b5eb7
yokomotod <yokomotod@gmail.com>
2025-04-12 20:04:35 +0900

    refactor: webview

--- client/src/executeQuery.ts
+++ client/src/executeQuery.ts
@@ -13,6 +13,11 @@ import { BQLS_COMMANDS, BQLS_METHOD_VIRTUAL_TEXT_DOCUMENT } from './constants';
 const COMMAND_SAVE_TO_CSV = 'saveToCsv';
 const COMMAND_SAVE_TO_SPREADSHEET = 'saveToSpreadsheet';
 
+interface Result {
+	columns: string[];
+	data: unknown[][];
+}
+
 export function buildExecuteQueryFunction(client: LanguageClient) {
 	return async () => {
 		const virtualTextDocument = await client.sendRequest<{
@@ -22,78 +27,88 @@ export function buildExecuteQueryFunction(client: LanguageClient) {
 			arguments: [window.activeTextEditor.document.uri.toString()],
 		});
 
-		const result = await client.sendRequest<{
-			result: { columns: string[]; data: unknown[][] };
+		const res = await client.sendRequest<{
+			result: Result;
 		}>(BQLS_METHOD_VIRTUAL_TEXT_DOCUMENT, {
 			textDocument: virtualTextDocument.textDocument,
 		});
 
-		const headers = result.result.columns
-			.map((col) => `<th>${col}</th>`)
-			.join('');
-		const rows = result.result.data
-			.map(
-				(row) => `<tr>${row.map((cell) => `<td>${cell}</td>`).join('')}</tr>`,
-			)
-			.join('');
-		const panel = window.createWebviewPanel(
-			'queryResult',
-			'Query Result',
-			ViewColumn.One,
-			{
-				enableScripts: true,
-			},
+		createWebviewPanel(
+			client,
+			virtualTextDocument.textDocument.uri,
+			res.result,
 		);
+	};
+}
 
-		panel.webview.html = `
-	<html lang="en">
-	<body>
-		<button id="saveToCsv">Save to CSV</button>
-		<button id="saveToSpreadsheet">Save to Spreadsheet</button>
-		<table border="1">
-			<thead>
-				<tr>${headers}</tr>
-			</thead>
-			<tbody>
-				${rows}
-			</tbody>
-		</table>
-		<script>
-			const vscode = acquireVsCodeApi();
-			document.getElementById('saveToCsv').addEventListener('click', () => {
-				vscode.postMessage({ command: '${COMMAND_SAVE_TO_CSV}' });
-				});
-			document.getElementById('saveToSpreadsheet').addEventListener('click', () => {
-				vscode.postMessage({ command: '${COMMAND_SAVE_TO_SPREADSHEET}' });
-			});
-		</script>
-	</body>
+function createWebviewPanel(
+	client: LanguageClient,
+	jobUri: string,
+	result: Result,
+) {
+	const panel = window.createWebviewPanel(
+		'queryResult',
+		'Query Result',
+		ViewColumn.One,
+		{
+			enableScripts: true,
+		},
+	);
+
+	const headers = result.columns.map((col) => `<th>${col}</th>`).join('');
+
+	const rows = result.data
+		.map((row) => `<tr>${row.map((cell) => `<td>${cell}</td>`).join('')}</tr>`)
+		.join('');
+
+	panel.webview.html = `
+<html lang="en">
+<body>
+<button id="saveToCsv">Save to CSV</button>
+<button id="saveToSpreadsheet">Save to Spreadsheet</button>
+<table border="1">
+	<thead>
+		<tr>${headers}</tr>
+	</thead>
+	<tbody>
+		${rows}
+	</tbody>
+</table>
+<script>
+	const vscode = acquireVsCodeApi();
+	document.getElementById('saveToCsv').addEventListener('click', () => {
+		vscode.postMessage({ command: '${COMMAND_SAVE_TO_CSV}' });
+		});
+	document.getElementById('saveToSpreadsheet').addEventListener('click', () => {
+		vscode.postMessage({ command: '${COMMAND_SAVE_TO_SPREADSHEET}' });
+	});
+</script>
+</body>
 </html>
 `;
 
-		panel.webview.onDidReceiveMessage(async (message) => {
-			if (message.command === COMMAND_SAVE_TO_CSV) {
-				const uri = await window.showSaveDialog({
-					saveLabel: 'Save',
-					filters: { 'CSV Files': ['csv'] },
+	panel.webview.onDidReceiveMessage(async (message) => {
+		if (message.command === COMMAND_SAVE_TO_CSV) {
+			const uri = await window.showSaveDialog({
+				saveLabel: 'Save',
+				filters: { 'CSV Files': ['csv'] },
+			});
+			if (uri) {
+				await client.sendRequest(ExecuteCommandRequest.method, {
+					command: BQLS_COMMANDS.SAVE_RESULT,
+					arguments: [jobUri, uri.toString()],
 				});
-				if (uri) {
-					await client.sendRequest(ExecuteCommandRequest.method, {
-						command: BQLS_COMMANDS.SAVE_RESULT,
-						arguments: [virtualTextDocument.textDocument.uri, uri.toString()],
-					});
-					window.showInformationMessage('Query result saved successfully!');
-				}
-			} else if (message.command === COMMAND_SAVE_TO_SPREADSHEET) {
-				const result = await client.sendRequest<{ url: string }>(
-					ExecuteCommandRequest.method,
-					{
-						command: BQLS_COMMANDS.SAVE_RESULT,
-						arguments: [virtualTextDocument.textDocument.uri, 'sheet://new'],
-					},
-				);
-				await env.openExternal(Uri.parse(result.url));
+				window.showInformationMessage('Query result saved successfully!');
 			}
-		});
-	};
+		} else if (message.command === COMMAND_SAVE_TO_SPREADSHEET) {
+			const result = await client.sendRequest<{ url: string }>(
+				ExecuteCommandRequest.method,
+				{
+					command: BQLS_COMMANDS.SAVE_RESULT,
+					arguments: [jobUri, 'sheet://new'],
+				},
+			);
+			await env.openExternal(Uri.parse(result.url));
+		}
+	});
 }
